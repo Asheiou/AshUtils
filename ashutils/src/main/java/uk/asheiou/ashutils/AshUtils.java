e001c47110f36ab991a56fab3cb4f3b693552d12
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-05-24 21:26:10 +0100

    AfkStatusChangeEvent discord broadcaster v1.0.1

--- ashutils/src/main/java/uk/asheiou/ashutils/AshUtils.java
+++ ashutils/src/main/java/uk/asheiou/ashutils/AshUtils.java
@@ -1,41 +1,52 @@
 package uk.asheiou.ashutils;
+
 import java.io.File;
 import java.time.Duration;
 import java.time.Instant;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import uk.asheiou.ashutils.command.*;
+import uk.asheiou.ashutils.listener.BukkitEventListener;
+import uk.asheiou.ashutils.listener.EssEventListener;
 import uk.asheiou.ashutils.restartonempty.ROEToggle;
 
 public final class AshUtils extends JavaPlugin {
-  
+
   @Override
   public void onEnable() {
     getLogger().info("Starting load.");
     Instant startTime = Instant.now();
-    
-    if(!EconManager.setupEconomy(this)) { 
-      getLogger().severe("Vault dependency not found! Disabling."); 
-      getServer().getPluginManager().disablePlugin(this);
-      }
-    
-    if(!new File(getDataFolder(), "config.yml").exists()) {
+    PluginManager pm = getServer().getPluginManager();
+    // // // // // // // // Events // // // // // // // //
+    pm.registerEvents(new BukkitEventListener(this), this);
+    if (pm.getPlugin("DiscordSRV") != null && pm.getPlugin("Essentials") != null) {
+      pm.registerEvents(new EssEventListener(this), this);
+    } else { 
+      getLogger().severe("Dependencies not found! Disabling.");
+      pm.disablePlugin(this);  
+    } 
+    // // // // // // // // Vault // // // // // // // //
+    if (!EconManager.setupEconomy(this)) {
+      getLogger().severe("Vault dependency not found! Disabling.");
+      pm.disablePlugin(this);
+    }
+    // // // // // // // // Config // // // // // // // //
+    if (!new File(getDataFolder(), "config.yml").exists()) {
       saveDefaultConfig();
       getLogger().info("Config file not found! Creating one.");
     }
-    
     getConfig().options().copyDefaults(true);
     saveConfig();
-    getServer().getPluginManager().registerEvents(new EventListener(this), this);
+    // // // // // // // // Commands // // // // // // // //
     this.getCommand("headsell").setExecutor(new HeadSellCommandExecutor(this));
-    
     this.getCommand("ashutils").setExecutor(new AshUtilsTabExecutor());
     this.getCommand("ashutils").setTabCompleter(new AshUtilsTabExecutor());
     getLogger().info("Commands and events registered.");
-    
+    // // // // // // // // RestartOnEmpty // // // // // // // //
     ROEToggle.setStatus(false);
-    
+
     Instant endTime = Instant.now();
     getLogger().info("\u001B[32mLoad complete in " + Duration.between(startTime, endTime).toMillis() + "ms.\u001B[0m");
   }
--- ashutils/src/main/java/uk/asheiou/ashutils/listener/BukkitEventListener.java
+++ ashutils/src/main/java/uk/asheiou/ashutils/listener/BukkitEventListener.java
@@ -1,4 +1,4 @@
-package uk.asheiou.ashutils;
+package uk.asheiou.ashutils.listener;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,14 +8,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import uk.asheiou.ashutils.restartonempty.ROEQuitTask;
 
-public class EventListener implements Listener {
-  
+public class BukkitEventListener implements Listener {
+
   private final JavaPlugin plugin;
-  
-  public EventListener(JavaPlugin plugin) { this.plugin = plugin; }
-  
+
+  public BukkitEventListener(JavaPlugin plugin) { this.plugin = plugin; }
+
   @EventHandler(priority = EventPriority.LOWEST)
-  public void onPlayerQuit(PlayerQuitEvent event) {
-    new ROEQuitTask().runTaskLater(this.plugin, 3);
-  }
+  public void onPlayerQuit(PlayerQuitEvent event) { new ROEQuitTask().runTaskLater(this.plugin, 3); }
 }
