b4fdac6b170f32fed222260c6afa0fafc698a709
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-07-01 01:26:07 +0100

    avoid exitout of function before it can reach motd logic

--- src/main/java/xyz/aeolia/ashutils/listener/BukkitEventListener.java
+++ src/main/java/xyz/aeolia/ashutils/listener/BukkitEventListener.java
@@ -53,9 +53,10 @@ public class BukkitEventListener implements Listener {
     if (refUUID != null) {
       if (refUUID.equals(event.getPlayer().getUniqueId())) {
         plugin.getLogger().info(event.getPlayer().getName() + " is already registered to users.json.");
-        return;
       }
-      plugin.getLogger().info(event.getPlayer().getName() + " is registered to users.json as another UUID! Correcting.");
+      else {
+        plugin.getLogger().info(event.getPlayer().getName() + " is registered to users.json as another UUID! Correcting.");
+      }
     } else {
       plugin.getLogger().info(event.getPlayer().getName() + " is not registered to users.json. Adding them.");
     }
