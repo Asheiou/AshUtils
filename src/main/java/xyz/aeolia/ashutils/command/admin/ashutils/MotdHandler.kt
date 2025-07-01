3a31b1605a9af3e661862162d706bc739d7d128f
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-07-01 16:33:36 +0100

    motdhandler kotlin

--- src/main/java/xyz/aeolia/ashutils/command/admin/ashutils/MotdHandler.kt
+++ src/main/java/xyz/aeolia/ashutils/command/admin/ashutils/MotdHandler.kt
@@ -1,31 +1,38 @@
-package xyz.aeolia.ashutils.command.admin.ashutils;
+package xyz.aeolia.ashutils.command.admin.ashutils
 
-import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.Nullable;
-import xyz.aeolia.ashutils.sender.MessageSender;
+import org.bukkit.command.CommandSender
+import xyz.aeolia.ashutils.sender.MessageSender
 
-public class MotdHandler {
-  public static String motd;
+class MotdHandler {
+  companion object {
+    @JvmStatic
+    private var motd : String? = null
 
-  public static String getMotd() {
-    return motd;
-  }
-
-  public static void setMotd(@Nullable String motdToSet) {
-    motd = motdToSet;
-  }
-
-  public static boolean handleCommand(CommandSender sender, String[] args) {
-    if (args.length == 0) {
-      setMotd(null);
-      MessageSender.sendMessage(sender, "MOTD reset!");
-    } else {
-      setMotd(String.join(" ", args));
-      MessageSender.sendMessage(sender, "MOTD set to: ");
-      MessageSender.sendMessage(sender, getMotd());
-      MessageSender.sendMessage(sender, "It will persist until the server restarts, unless you reset it by " +
-              "running this command again without an argument.");
+    @JvmStatic
+    fun getMotd() : String?{
+      return motd
     }
-    return true;
+
+    @JvmStatic
+    fun setMotd(motd: String?) {
+      this.motd = motd
+    }
+
+    @JvmStatic
+    fun handleCommand(sender: CommandSender, args: Array<out String?>?): Boolean {
+      if (args?.size == 0 || args == null) {
+        setMotd(null)
+        MessageSender.sendMessage(sender, "MOTD reset!")
+        return true
+      }
+      setMotd(args.joinToString(" "))
+      MessageSender.sendMessage(sender, "Motd set to:")
+      MessageSender.sendMessage(sender, getMotd())
+      MessageSender.sendMessage(
+        sender, "It will persist until the server restarts or you reset it by " +
+                "running this command again without an argument."
+      )
+      return true
+    }
   }
 }
