d51e9eca03e6c8c8bea4b7171d64cff5eb3335cd
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-07-01 01:05:58 +0100

    rewrite MessageSender to support sending clickables

--- src/main/java/xyz/aeolia/ashutils/sender/MessageSender.java
+++ src/main/java/xyz/aeolia/ashutils/sender/MessageSender.java
@@ -1,6 +1,9 @@
 package xyz.aeolia.ashutils.sender;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.aeolia.ashutils.AshUtils;
@@ -10,8 +13,10 @@ public class MessageSender {
   public static void sendMessage(CommandSender recipient, String message, boolean includePrefix) {
     String prefix = JavaPlugin.getProvidingPlugin(AshUtils.class).getConfig().getString("chat-prefix");
     if (includePrefix) message = prefix + "<reset> " + message;
-    Component deserialized = MiniMessageManager.getMiniMessage().deserialize(message);
-    MiniMessageManager.adventure().sender(recipient).sendMessage(deserialized);
+    Component adventureComponent = MiniMessageManager.getMiniMessage().deserialize(message);
+    String json = GsonComponentSerializer.gson().serialize(adventureComponent);
+    BaseComponent[] spigotComponents = ComponentSerializer.parse(json);
+    recipient.spigot().sendMessage(spigotComponents);
   }
 
   public static void sendMessage(CommandSender recipient, String message) {
